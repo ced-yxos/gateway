84a6647af5e64b195c5672880d79b1a89c668882
ced-yxos <jean.cedricsanou@gmail.com>
2024-03-06 18:13:38 +0100

    add service_deployment

--- gateway.py
+++ gateway.py
@@ -57,13 +57,3 @@ async def set_service_endpoint(data: dict):
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
     print("server running")
-
-
-    
-
-
-    
-    
-
-    
-
